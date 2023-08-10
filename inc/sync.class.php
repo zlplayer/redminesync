071a71875c43773424f7e94608454f6ef6d3fd68
Konrad Boroń <konrad.boron@op.pl>
2023-08-10 14:43:01 +0200

    validation and uploading to the server

--- inc/sync.class.php
+++ inc/sync.class.php
@@ -141,10 +141,10 @@ class PluginRedminesyncSync extends CommonGLPI
         $date_creation = date('Y-m-d H:i:s', strtotime($data->created_on));
 
         $inserted_project_id = $DB->request("SELECT project_id FROM glpi_plugin_redminesync_synclog WHERE rm_project_id=$redmine_id LIMIT 1");
-        foreach($inserted_project_id as $id){
+        foreach ($inserted_project_id as $id) {
             $project_id = $id['project_id'];
         }
-        
+
         $create_project_sql = "UPDATE glpi_projects SET name='$name', content='$content' WHERE id='$project_id'";
         $DB->query($create_project_sql);
 
@@ -156,11 +156,10 @@ class PluginRedminesyncSync extends CommonGLPI
         foreach ($tickets_dates as $date) {
             $inserted_ticket_dates[] = $date['date'];
         }
-        if (!in_array($date_creation,$inserted_ticket_dates)) {
+        if (!in_array($date_creation, $inserted_ticket_dates)) {
             $create_ticket = "INSERT INTO glpi_tickets SET id='$project_id', name='$name', `date`='$date_creation', content='$content'";
             $DB->query($create_ticket);
         }
-
     }
 
     // to sync projects
@@ -211,7 +210,7 @@ class PluginRedminesyncSync extends CommonGLPI
 
         $ticket_id = self::getTicketIdByIssueId($issue->id);
 
-        // Adres URL do pobrania komentarzy dla danego zgłoszenia w Redmine
+        //URL for comments from Redmine
         $url = self::$config['url'] . '//issues/' . $issue->id . '.json?include=journals,attachments&key=' . self::$config['key'];
 
         $ch = curl_init();
@@ -228,10 +227,72 @@ class PluginRedminesyncSync extends CommonGLPI
                 if (!$notes == ''  && !self::checkIfNoteExistsInGlpi($journal->created_on)) {
                     $created_at = date('Y-m-d H:i:s', strtotime($journal->created_on));
 
-                    // Utwórz nową wiadomość dla komentarza
+                    //Create new massage
                     $add_message_sql = "INSERT INTO glpi_ticketfollowups SET tickets_id='$ticket_id', date='$created_at', users_id='2', content='$notes', is_private='1'";
                     $DB->query($add_message_sql);
                 }
+            }
+        }
+        if (isset($result->issue->attachments)) {
+            $redmine_files = [];
+            foreach ($result->issue->attachments as $attachment) {
+                //Check if file exists with given link
+                if (self::checkIfFileExists($attachment->content_url)) {
+                    $redmine_files[] = [
+                        'filename' => $attachment->filename,
+                        'link' => $attachment->content_url,
+                        'mime' => $attachment->content_type,
+                        'comment' => $attachment->description,
+                        'date_creation' => date('Y-m-d H:i:s', strtotime($attachment->created_on))
+                    ];
+                }
+            }
+            self::mapFilesRedmineToGlpi($redmine_files, $ticket_id);
+        }
+    }
+
+    // Funkcja sprawdzająca, czy plik istnieje
+    static function checkIfFileExists($file_url)
+    {
+        $ch = curl_init($file_url);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
+        curl_exec($ch);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return $status == 200;
+    }
+
+    // Function mapping files from Redmine to GLPI
+    static function mapFilesRedmineToGlpi($redmine_files, $ticket_id)
+    {
+        global $DB;
+
+        //Check if file already exists in GLPI database
+        $inserted_files = $DB->request("SELECT link FROM glpi_documents");
+        $files = array();
+        foreach ($inserted_files as $inserted_file) {
+            $files[] = $inserted_file['link'];
+        }
+
+        foreach ($redmine_files as $redmine_file) {
+            if (!in_array($redmine_file['link'], $files)) {
+                
+                $document_id = $DB->insert_id();
+                $data = file_get_contents($redmine_file['link']);
+                $localFilePath = '/var/www/glpi-2support.nomino.pl/files/TXT/29/' .$document_id. $redmine_file['filename'];
+                file_put_contents($localFilePath, $data);
+
+                $filepath = "TXT/29/".$document_id. $redmine_file['filename'];
+                $glpi_insert_query = "INSERT INTO glpi_documents SET name='',filepath='$filepath' ,filename ='{$redmine_file['filename']}', link='{$redmine_file['link']}',
+                mime='{$redmine_file['mime']}',comment='{$redmine_file['comment']}',date_creation='{$redmine_file['date_creation']}', tickets_id='$ticket_id',users_id='2'";
+
+                $DB->query($glpi_insert_query);
+
+                $last_document_id = $DB->insert_id();
+
+                $glpi_insert_item_query = "INSERT INTO glpi_documents_items (documents_id, items_id, itemtype, entities_id, is_recursive, date_mod, users_id, timeline_position)
+                VALUES ('$last_document_id', '$ticket_id', 'Ticket', 1, 0, NOW(), 2, 0)";
+                $DB->query($glpi_insert_item_query);
             }
         }
     }
